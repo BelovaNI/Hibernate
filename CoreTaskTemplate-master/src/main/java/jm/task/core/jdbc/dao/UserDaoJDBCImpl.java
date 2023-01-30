bca1d9b62cb9803e2f5a0acca82077290e73715e
BelovaNI <belova.ni@inbox.ru>
2023-01-30 19:55:05 +0500

    30/01/2023 var

--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -16,8 +22,18 @@ public class UserDaoJDBCImpl implements UserDao {
     public void dropUsersTable() {
 
     }
-
+    @Override
     public void saveUser(String name, String lastName, byte age) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users(name, lastmame, age) VALUES  (?, ?, ?)")) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(1, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
 
     }
 
@@ -25,8 +41,26 @@ public class UserDaoJDBCImpl implements UserDao {
 
     }
 
+    @Override
     public List<User> getAllUsers() {
-        return null;
+        List <User> users = new ArrayList<>();
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT  * FROM users")) {
+             ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                String lastname = rs.getString("last name");
+                byte age = rs.getByte("age");
+
+                users.add(new User(name, lastname, age));
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
--- CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/util/Util.java
+++ CoreTaskTemplate-master/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/database_1";
+    private final static String USER_NAME = "root";
+    private final static String PASSWORD = "1Donnet!";
+
+
+    public static Connection getConnection () {
+        Connection connection = null;
+        try {
+            connection =DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+        }catch (SQLException ex) {
+            System.out.println("Connection error");
+        }
+        return connection;
+    }
 }
